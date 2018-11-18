fff5af44fb1f6e57849b70704cd6edf5b271f7a0
RiCK BROWN <ricky.brown.00@gmail.com>
2018-11-18 00:13:34 +0000

    Gameboard functioning

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      gameboard: [
+                  false, false, false, false, false, false, false, 
+                  false, false, false, false, false, false, false,
+                  false, false, false, false, false, false, false,
+                  false, false, false, false, false, false, false,
+                  false, false, false, false, false, false, false,
+                  false, false, false, false, false, false, false
+                ]
+    }
+  }
+
+  placeCounter = index => {
+    const gameboard = this.state.gameboard
+    const changedState = [gameboard[index] = !gameboard[index]]
+    this.setState({ ...gameboard, changedState})
+  }
+
   render() {
+    const gameboard = this.state.gameboard
     return (
-      <div>
-        app
+      <div className="grid-container">
+        {gameboard.map((square, index) => {
+          return(
+            <div key={index} className="grid" onClick={() => this.placeCounter(index)}>{square ? <div className="counter"></div> : <div className="blank"></div>}</div>
+          )
+        })}
       </div>
     )
   }
