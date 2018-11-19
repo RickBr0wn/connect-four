d410bf6b28f28a2e22e36f1fa14ceb3bbeda9e4d
RiCK BROWN <ricky.brown.00@gmail.com>
2018-11-19 23:16:45 +0000

    Alternate turns working. Falling method needs logic

--- src/App.js
+++ src/App.js
@@ -1,35 +1,103 @@
 import React, { Component } from 'react'
+import { PLAYER_1, PLAYER_2, EMPTY } from './Constants/gameboardConstants'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      whosTurnIsIt: PLAYER_1,
       gameboard: [
-                  false, false, false, false, false, false, false, 
-                  false, false, false, false, false, false, false,
-                  false, false, false, false, false, false, false,
-                  false, false, false, false, false, false, false,
-                  false, false, false, false, false, false, false,
-                  false, false, false, false, false, false, false
+                  EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, 
+                  EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
+                  EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
+                  EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
+                  EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
+                  EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY
                 ]
     }
   }
 
-  placeCounter = index => {
-    const gameboard = this.state.gameboard
-    const changedState = [gameboard[index] = !gameboard[index]]
-    this.setState({ ...gameboard, changedState})
+  whichCounter = index => {
+    switch(this.state.gameboard[index]) {
+      case PLAYER_1:
+        return <div className="counter red"></div>
+      case PLAYER_2:
+        return <div className="counter yellow"></div>
+      case EMPTY:
+        return <div className="counter blank"></div>
+      default:
+        return <div className="counter blank"></div>
+    }
+  }
+
+  whosTurn = whosTurnIsItNow => {
+    switch(whosTurnIsItNow) {
+      case PLAYER_1:
+        return PLAYER_2
+      case PLAYER_2:
+        return PLAYER_1
+      case EMPTY:
+        return PLAYER_1
+      default:
+        return EMPTY
+    }
+  }
+
+  handleClick = indexToBeChanged => {
+    const whosTurnIsItNow = this.state.whosTurnIsIt
+    const oldGameBoard = this.state.gameboard
+    const whosTurnIsIt = this.whosTurn(whosTurnIsItNow)
+    const newGameboard = oldGameBoard.map((spot, index) => {
+      if(index === indexToBeChanged) {
+        spot = whosTurnIsItNow
+      }
+      return spot
+    })
+    const gameboard = this.falling(newGameboard, indexToBeChanged, whosTurnIsIt)
+    console.log(gameboard)
+    this.setState({ 
+      gameboard,
+      whosTurnIsIt
+    })
+  } 
+
+  playersTurnCounter = () => {
+    const redCounter = <div className="counter red"></div>
+    const yellowCounter = <div className="counter yellow"></div>
+    if(this.state.whosTurnIsIt === PLAYER_1) {
+      return redCounter
+    }else {
+      return yellowCounter
+    }
+  }
+
+  falling = (arr, position, constant) => {
+    const widthOfGrid = 7
+    const orignalPosition = position
+    for(let i = position; i < arr.length; i += widthOfGrid) {
+      if(arr[position + widthOfGrid] === EMPTY) {
+        i += widthOfGrid
+      }
+    }
+    arr[position] = constant
+    return arr
   }
 
   render() {
     const gameboard = this.state.gameboard
+    const whosTurnIsIt = this.state.whosTurnIsIt
     return (
-      <div className="grid-container">
-        {gameboard.map((square, index) => {
-          return(
-            <div key={index} className="grid" onClick={() => this.placeCounter(index)}>{square ? <div className="counter"></div> : <div className="blank"></div>}</div>
-          )
-        })}
+      <div>
+        <h1>Connect 4</h1>
+        <p>{whosTurnIsIt}: {this.playersTurnCounter()}</p>
+        <div className="grid-container">
+          {gameboard.map((square, index) => {
+            const counter = this.whichCounter(index)
+            return(
+              <div key={index} className="grid" onClick={() => this.handleClick(index)}>{counter}</div>
+            )
+          })}
+        </div>  
       </div>
     )
   }
---
+++ src/Constants/gameboardConstants.jsx
@@ -0,0 +1,3 @@
+export const EMPTY = 'EMPTY'
+export const PLAYER_1 = 'PLAYER_1'
+export const PLAYER_2 = 'PLAYER_2'
